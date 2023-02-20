e77b754203ce821787a9b735e37cb52f3256e10c
Abdullah-Sakib <abdullahsakib888@gmail.com>
2023-02-20 14:10:21 +0600

    calculator

---
+++ README.md
@@ -0,0 +1,8 @@
+# This is a simple calculator
+
+Live link: [Visit here](https://snislam-simple-calculator.netlify.app/)
+
+Direct link: https://snislam-simple-calculator.netlify.app/
+
+You can calculate only +, -, X, / and percentage(%)
+
---
+++ src/App.js
@@ -0,0 +1,157 @@
+import { useEffect, useState } from "react";
+import "./styles/app.css";
+
+function App() {
+  const [value, setValue] = useState("");
+  const [number, setNumber] = useState([]);
+  const [string, setString] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("message", function (e) {
+      if (e.origin !== "http://localhost:3000") return;
+      setValue(e.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const sendMessage = () => {
+      window.parent.postMessage([number, string], "http://localhost:3000");
+    };
+    sendMessage();
+  }, [number, string]);
+
+  // whenever click button of digit
+  const handleClick = (val) => {
+    if (val == "=") {
+      let statement = "";
+      const fSatetement = statement + value;
+
+      // for percentage
+      if (fSatetement.includes("%")) {
+        const pre = fSatetement.split("%")[0];
+        const post = fSatetement.split("%")[1];
+        const calc = Number(pre) * (Number(post) / 100);
+        setValue(calc);
+      } else {
+        // for normal calculation
+        setValue(eval(fSatetement));
+      }
+    } else {
+      let num = `${value}` + val;
+      setValue(num);
+    }
+
+    // check and set values to the respective states
+    if (typeof val === "number") {
+      setNumber([...number, val]);
+    } else {
+      setString(val);
+    }
+  };
+
+  // clear function
+  const handleClear = () => {
+    setValue("");
+  };
+  // delete function
+  const handleDelete = () => {
+    if (value) {
+      setValue(value.slice(0, value.length - 1));
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="calculator-main-body">
+        <div className="calculator--body">
+          <div className="calculator--body--display">
+            <input value={value} readOnly placeholder="Child" />
+            {/* <input value={recievedMessage} /> */}
+          </div>
+          <div className="calculator--body--keypad">
+            <div className="row">
+              <button
+                onClick={() => handleClear("AC")}
+                disabled={!value}
+                className="btn"
+              >
+                AC
+              </button>
+              <button
+                onClick={() => handleDelete("x")}
+                disabled={!value}
+                className="btn"
+              >
+                x
+              </button>
+              <button onClick={() => handleClick("%")} className="btn">
+                %
+              </button>
+              <button onClick={() => handleClick("/")} className="btn">
+                /
+              </button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClick(7)} className="btn">
+                7
+              </button>
+              <button onClick={() => handleClick(8)} className="btn">
+                8
+              </button>
+              <button onClick={() => handleClick(9)} className="btn">
+                9
+              </button>
+              <button onClick={() => handleClick("*")} className="btn">
+                X
+              </button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClick(4)} className="btn">
+                4
+              </button>
+              <button onClick={() => handleClick(5)} className="btn">
+                5
+              </button>
+              <button onClick={() => handleClick(6)} className="btn">
+                6
+              </button>
+              <button onClick={() => handleClick("-")} className="btn">
+                -
+              </button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClick(1)} className="btn">
+                1
+              </button>
+              <button onClick={() => handleClick(2)} className="btn">
+                2
+              </button>
+              <button onClick={() => handleClick(3)} className="btn">
+                3
+              </button>
+              <button onClick={() => handleClick("+")} className="btn">
+                +
+              </button>
+            </div>
+            <div className="row">
+              <button onClick={() => handleClick("//")} className="btn">
+                \\
+              </button>
+              <button onClick={() => handleClick(0)} className="btn">
+                0
+              </button>
+              <button onClick={() => handleClick(".")} className="btn">
+                .
+              </button>
+              <button onClick={() => handleClick("=")} className="btn">
+                =
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
